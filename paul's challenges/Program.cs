159a927829bb83bfa1530c853ca48f64d7b0c3a4
Ian Stewart <iacostewart@msn.com>
2018-05-17 10:19:10 -0400

    UPPERCASE name arry to lowerCase name list

---
+++ paul's challenges/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[] { "GEORGE", "PAUL", "JOHN", "RINGO" };
+
+            foreach (string name in names)
+            {
+                string[] lowername = new string[] { (name.ToLower()) };
+                //foreach (string lower in lowername);
+                List<string> lowercase = new List<string>();
+                foreach (string lname in lowername)
+                {
+                    lowercase.Add(lname);
+                }
+                foreach(string n in lowername)
+                {
+                    Console.WriteLine(n);
+                }
+            }
+
+
+        }
+    }
+}
